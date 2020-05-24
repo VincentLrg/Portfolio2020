8c6862e0c36eceb0cf826145cfadc3f2f8fc4644
VincentLrg_ <lrgl.vincent@gmail.com>
2020-05-24 20:00:09 +0200

    First commit

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    css: {
+      loaderOptions: {
+        sass: {
+            prependData: `
+              @import "src/assets/scss/app.scss";
+              @import "src/assets/scss/fonts.scss";
+              @import "src/assets/scss/variables.scss";
+            `
+          }
+      }
+    },
+    configureWebpack: {
+        module: {
+          rules: [{
+            test: /\.(ttf|otf|eot|woff|woff2)$/,
+            use: {
+              loader: "file-loader",
+              options: {
+                name: "src/assets/fonts/[name].[ext]",
+              },
+            },
+          }]
+        }
+      }
+  };
